bb498982bdd8ae46042103df34127c7635abae47
Alex Weinberger <alexweinberger7@gmail.com>
2018-11-28 17:11:10 -0500

    iterating through directories

--- main.py
+++ main.py
@@ -1,29 +1,68 @@
 import tensorflow as tf
+import os
 
 
+def parse_image(img):
 
-def parse_image(img, label):
     decoded = tf.image.decode_jpeg(img)
     resized = tf.image.resize_images(decoded, [28, 28])
     greyscale = tf.image.rgb_to_grayscale(resized)
 
-    with tf.Session() as sess:
-        # img1 = sess.run(resized)
-        # print(img1.shape)
-        # print(img1)
+    # TO SEE GREYSCALE VALUES UNCOMMENT
+    # with tf.Session() as sess:
+    #
+    #     greyscale_img = sess.run(greyscale)
+    #     print(greyscale_img)
 
-        greyscale = sess.run(greyscale)
-        return greyscale, label
+    return greyscale
 
+
+def iterate_through_directories(rootdir):
+    directory = os.fsencode(rootdir)
+
+    # list = [name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name))]
+
+    # for subdir in os.listdir(directory):
+    #     if os.path.isdir(os.path.join(directory, subdir)):
+    #         basename = os.path.basename(subdir)
+    #         for file in subdir:
+    #             print(basename, file)
+
+    images = []
+    labels = []
+    for path, dirs, files in os.walk(directory):
+
+        base_name = os.path.basename(path)
+
+        for file in files:
+            img = tf.read_file(file)
+            images.append(img)
+            labels.append(base_name)
+
+    return images, labels
 
 
 def main():
-    # rootdir = "../test_set"
-    file = "1967.14.11.a.jpg"
-    img = tf.read_file(file)
-    label = "Test"
-    greyscale, label = parse_image(img, label)
-    print(greyscale)
+
+    rootdir = "hw-4-images/training_set"
+    images, labels = iterate_through_directories(rootdir)
+
+
+    greyscale = parse_image(images[2])
+
+    original_len = len(images)
+    images = [parse_image(img) for img in images]
+
+    print("original len", original_len)
+    print("new len", len(images))
+
+
+
+
+    # img = tf.read_file(file)
+    # label = "Test"
+    # greyscale, label = parse_image(img, label)
+    # print(greyscale)
 
 if __name__ == "__main__":
     main()
